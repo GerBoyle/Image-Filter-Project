6a2b7c4660ce6ea1e3cc98a3fa724afde897a796
GerBoyle <ger_boyle@yahoo.ie>
2020-09-21 22:45:07 +0100

    docs: Updated Readme with description of deployment url

--- src/server.ts
+++ src/server.ts
@@ -69,7 +69,7 @@ import {
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: express.Request, res:express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
